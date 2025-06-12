aabdf9840c249b92b645621bd8f7505ba41bc371
gwicho38 <luis@lefv.io>
2025-06-12 10:50:52 -0400

    Fix which-key config and register Copilot group

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,3 +1,8 @@
 return {
-  "https://github.com/github/copilot.vim",
+  "github/copilot.vim",
+  keys = {
+    { "<leader>ce", "<cmd>Copilot enable<cr>", desc = "Copilot Enable" },
+    { "<leader>cd", "<cmd>Copilot disable<cr>", desc = "Copilot Disable" },
+    { "<leader>cp", "<cmd>Copilot panel<cr>", desc = "Copilot Panel" },
+  },
 }
