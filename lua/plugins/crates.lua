7c192b27927ac027436064d8f1fad197d019effe
lefv <luis@lefv.io>
2025-09-30 23:15:15 +0200

    fix: update crates.nvim popup keys configuration
    Removed invalid 'expand' key and updated to proper key configuration format.
    All keys now use table format and include standard popup navigation keys:
    - hide: q, <esc>
    - select: <cr>
    - copy_value: yy (changed from <C-y>)
    - Added goto_item, jump_forward, jump_back for better navigation

--- lua/plugins/crates.lua
+++ lua/plugins/crates.lua
@@ -12,10 +12,17 @@ return {
       popup = {
         autofocus = true,
         keys = {
-          expand = "<CR>",
-          copy_value = "<C-y>",
-        }
-      }
+          hide = { "q", "<esc>" },
+          open_url = { "<cr>" },
+          select = { "<cr>" },
+          select_alt = { "s" },
+          toggle_feature = { "<cr>" },
+          copy_value = { "yy" },
+          goto_item = { "gd", "K" },
+          jump_forward = { "<c-i>" },
+          jump_back = { "<c-o>", "<c-p>" },
+        },
+      },
     })
   end,
 }
