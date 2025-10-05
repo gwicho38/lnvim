3d45699e4765b8b0b0134eb544513497c41930a7
lefv <luis@lefv.io>
2025-10-06 00:37:07 +0200

    feat: enable drag-and-drop image support with auto-save
    Configure img-clip.nvim with:
    - Drag-and-drop support in insert mode
    - Auto-save images to ./assets/ directory
    - Auto-insert markdown reference with preview
    - URL encoding for image paths

--- lua/plugins/img-clip.lua
+++ lua/plugins/img-clip.lua
@@ -2,11 +2,27 @@ return {
   "HakonHarnes/img-clip.nvim",
   event = "VeryLazy",
   opts = {
-    -- add options here
-    -- or leave it empty to use the default settings
+    default = {
+      embed_image_as_base64 = false,
+      prompt_for_file_name = false,
+      drag_and_drop = {
+        enabled = true,
+        insert_mode = true,
+      },
+      use_absolute_path = false,
+    },
+    filetypes = {
+      markdown = {
+        url_encode_path = true,
+        template = "![$CURSOR]($FILE_PATH)",
+        dir_path = function()
+          -- Save to ./assets relative to current file
+          return "assets"
+        end,
+      },
+    },
   },
   keys = {
-    -- suggested keymap
-    { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+    { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from clipboard" },
   },
 }
