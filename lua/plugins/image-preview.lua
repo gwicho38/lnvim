2219c7ba636041ab6025dd59ca981bf26e2ca514
lefv <luis@lefv.io>
2025-10-06 00:58:52 +0200

    fix: add tmux support and debug command for image.nvim
    Add hijack_file_patterns and ImageDebug command to troubleshoot
    image preview issues. Configure tmux compatibility.

--- lua/plugins/image-preview.lua
+++ lua/plugins/image-preview.lua
@@ -17,5 +17,16 @@ return {
     max_height_window_percentage = 50,
     window_overlap_clear_enabled = true,
     window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+    tmux_show_only_in_active_window = true,
+    hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp" },
   },
+  config = function(_, opts)
+    require("image").setup(opts)
+
+    -- Debug: check if backend is working
+    vim.api.nvim_create_user_command("ImageDebug", function()
+      local img = require("image")
+      vim.notify("Image.nvim backend: " .. (img.backend and img.backend.name or "none"), vim.log.levels.INFO)
+    end, {})
+  end,
 }
